0ae6766d66d7c05f27c2e9e592a45ef11c615d31
Joel Byler <jbyler@covermymeds.com>
2016-10-07 15:33:08 -0400

    Use production config from Heroku ENV variables and decrease socket timeout

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,7 +6,8 @@ defmodule Sketchpad.UserSocket do
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket,
-    check_origin: false
+    check_origin: false,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
